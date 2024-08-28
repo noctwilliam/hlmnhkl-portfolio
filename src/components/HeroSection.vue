c7661bda46eaeb528240e2022e2f6de1391b9c62
hilmanhaikal <hilmanhaikal7@gmail.com>
2024-08-29 02:37:07 +0800

    modify

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -2,7 +2,7 @@
   <section id="hero" class="bg-blue-500 text-white text-center py-20">
     <h1 class="text-4xl font-bold text-black">Hi, I'm Hilman Haikal</h1>
     <p class="text-xl mt-4 text-black">A passionate software developer and designer.</p>
-    <a href="#projects" class="mt-8 inline-block bg-black text-white py-2 px-4 rounded">See My Work</a>
+    <a href="#projects" class="mt-8 inline-block font-bold bg-black text-white py-2 px-4 rounded">See My Work</a>
   </section>
 </template>
 
